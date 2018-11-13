7873bf8f200ab4fdc8e65eedd6797f9239db0df0
lhhong <heok.hong.low@gmail.com>
2018-11-13 23:26:26 +0800

    Add endpoint for db query

--- app.py
+++ app.py
@@ -17,6 +17,24 @@ def plot_query_tree():
     stats, all_nodes = plotQueryTree(data["qep"])
     return jsonify(stats=stats, all_nodes=all_nodes)
 
+@app.route("/plot_query_tree_from_sql", methods=["POST"])
+def plot_query_tree_from_sql():
+    data = request.get_json()
+
+    try:
+        conn = psycopg2.connect(host=data['host'],
+                                database=data['database'],
+                                user=data['user'],
+                                password=data['password'])
+        cur = conn.cursor()
+        cur.execute('EXPLAIN(FORMAT JSON) ' + data['query'])
+        result = cur.fetchall()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        cur.close()
+
+    print(result)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
