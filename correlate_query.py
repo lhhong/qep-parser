257f64a082d2997ff97c2da7e809ad7e89c479df
signapoop <leowyaoyang@gmail.com>
2018-11-13 23:31:13 +0800

    parse tree

--- correlate_query.py
+++ correlate_query.py
@@ -1,12 +1,27 @@
 from findExpressions import *
 from BooleanExpression import BooleanExpression
 
+
+def stripParenthesis(foo):
+    if foo[0]=='(' and foo[-1]==')':
+        return foo[1:-1]
+
+
+def findMatchingBE(query, booleans, cond):
+    for b in booleans:
+        be = BooleanExpression(cond, variables=b.variables, rev_variables=b.rev_variables)
+        if isEquivalentBooleanExpression(be, b):
+            return b.start, b.end, query[b.start:b.end]
+    return -1, -1, ""
+
+
 def reverseComparatorIfNeeded(comparator):
     if comparator == '<':
         return '>'
     elif comparator == '<=':
         return '>='
     return comparator
+
 
 def isEquivalentBooleanExpression(a,b):
     if a.left.exp==b.left.exp and a.right.exp==b.right.exp:
@@ -16,6 +31,7 @@ def isEquivalentBooleanExpression(a,b):
     
     return False
 
+
 def getMatchingClause(query, search_key):
     start = query.find(search_key)
     if start == -1:
@@ -24,11 +40,13 @@ def getMatchingClause(query, search_key):
     parts = shortened_query.split(' ') 
     return search_key + ' ' + parts[0]
 
+
 def findClauseStartEnd(query, clause):
     start = query.find(clause)
     if start == -1:
         return -1, -1, ""
     return start, start + len(clause), clause
+
 
 def correlateQuery(all_nodes, query):
     # Change query to a list of Boolean expressions
@@ -42,29 +60,29 @@ def correlateQuery(all_nodes, query):
             # Find hash condition as boolean expression
             cond = stripParenthesis(node.description_dict['Hash Cond'])
             s, e, match = findMatchingBE(query, booleans, cond)
-            print('Join:', cond, 'Matched: ', match)
+            #print('Join:', cond, 'Matched: ', match)
             
         elif 'Scan' in node.title:
             # Find relation name
             clause = " ".join([node.description_dict['Relation Name'], node.description_dict['Alias']])
             s, e, match = findClauseStartEnd(query, clause)
-            print('Scan:', clause, 'Matched: ', match)
+            #print('Scan:', clause, 'Matched: ', match)
             if 'Filter' in node.description_dict:
                 # Find filter condition as boolean expression
                 cond = stripParenthesis(node.description_dict['Filter'])
                 s, e, match = findMatchingBE(query, booleans, cond)
-                print('Filter:', cond, 'Matched: ', match)
+                #print('Filter:', cond, 'Matched: ', match)
                 
         elif 'Aggregate' in node.title:
             # Find GROUP BY
             clause = getMatchingClause(query, 'GROUP BY')
             s, e, match = findClauseStartEnd(query, clause)
-            print('Aggregate:', 'Matched: ', match)
+            #print('Aggregate:', 'Matched: ', match)
         
         elif 'Sort' in node.title:
             clause = getMatchingClause(query, 'ORDER BY')
             s, e, match = findClauseStartEnd(query, clause)
-            print('Sort:', 'Matched: ', match)
+            #print('Sort:', 'Matched: ', match)
             
         node.description_dict['StartOfQuery'] = s
         node.description_dict['EndOfQuery'] = e
